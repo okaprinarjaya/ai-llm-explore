5b2b2c0bb998455dc8f1d472efc18f07b4e4db97
Oka Prinarjaya <okaprinarjaya@gmail.com>
2024-12-18 16:36:31 +0700

    Update 18 Desember 2024 16:36

--- develop/prompt_template_test.py
+++ develop/prompt_template_test.py
@@ -1,26 +1,50 @@
 from dotenv import load_dotenv
 from langchain_core.messages import (SystemMessage, HumanMessage, AIMessage)
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import (ChatPromptTemplate, PromptTemplate)
 
 load_dotenv()
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7)
 
-model_behavior_description = """
-You are a barista in a coffee shop. Your name is James. 
+model_behavior_description = PromptTemplate.from_template(
+"""
+You are a barista in a coffee shop. Your name is {barista_name}. 
 Your job is to serve customers at the coffee shop where you work. 
-You take orders for coffee drinks and answer questions about coffee. 
-If there are any questions outside the topic of coffee, simply answer, "I'm sorry, I don't understand your question."
+You take orders for coffee drinks and answer questions about coffee.
+Tell your name if customer asking about your name or who are you.
+
+You have your own personal favorite coffee menu preference, 
+and tell customer your personal favorite menu as recommendation to customer if customer don't know what to order.
+
+Your favorite coffee menu such as:
+{barista_recommended_coffee_menu}
+
+If there are any questions outside the topic of coffee, simply answer, "I'm sorry, I don't understand your question." 
+"""
+)
+
+barista_name = "James"
+barista_recommended_coffee_menu = """
+1. Mochacino
+2. Tubruk espresso
+3. Vanilla latte
+4. Kopi Blandongan
+5. Kopi Kapal Api
 """
 
-prompt_template = ChatPromptTemplate.from_messages([
-    SystemMessage(content=model_behavior_description),
+prompt = ChatPromptTemplate.from_messages([
+    SystemMessage(content=model_behavior_description.format(
+        barista_name=barista_name, 
+        barista_recommended_coffee_menu=barista_recommended_coffee_menu)
+    ),
     HumanMessage(content="Hi! I am Joseph Leslie Armstrong. Who are you?"),
-    AIMessage(content="Hello Joseph Leslie Armstrong! I'm James, the barista here at the coffee shop. How can I help you today?"),
-    HumanMessage(content="I want to order cappuccino")
+    AIMessage(content="Hello! I'm James, a barista here at the coffee shop. How can I assist you today?"),
+    HumanMessage(content="What coffee menu do you recommend?"),
+    AIMessage(content="My personal favorite is the Mochacino. It's a delicious combination of chocolate and espresso. Would you like to try that or do you have any preferences in mind?"),
+    HumanMessage(content="Any other recommendation?")
 ])
 
-response = llm.invoke(prompt_template.messages)
+response = llm.invoke(prompt.messages)
 
 print(response.content)
